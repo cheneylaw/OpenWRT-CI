08c6da20fd65b50afe748f83d84557cfa8f6273b
cheneylaw <35223665+cheneylaw@users.noreply.github.com>
2024-09-17 15:56:36 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -30,7 +30,8 @@ fi
 #配置文件修改
 echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
 echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
-
+echo "CONFIG_PACKAGE_luci-app-ssr-plus=y" >> ./.config
+echo "CONFIG_PACKAGE_luci-app-turboacc=y" >> ./.config
 #手动调整的插件
 if [ -n "$WRT_PACKAGE" ]; then
 	echo "$WRT_PACKAGE" >> ./.config
@@ -45,13 +46,13 @@ if [[ $WRT_TARGET == *"IPQ"* ]]; then
 fi
 
 #科学插件设置
-if [[ $WRT_REPO == *"lede"* ]]; then
-	echo "CONFIG_PACKAGE_luci-app-openclash=n" >> ./.config
-	echo "CONFIG_PACKAGE_luci-app-passwall=n" >> ./.config
-	echo "CONFIG_PACKAGE_luci-app-ssr-plus=y" >> ./.config
-	echo "CONFIG_PACKAGE_luci-app-turboacc=y" >> ./.config
-else
-	echo "CONFIG_PACKAGE_luci=y" >> ./.config
-	echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
-	echo "CONFIG_PACKAGE_luci-app-homeproxy=y" >> ./.config
-fi
+#if [[ $WRT_REPO == *"lede"* ]]; then
+#	echo "CONFIG_PACKAGE_luci-app-openclash=n" >> ./.config
+#	echo "CONFIG_PACKAGE_luci-app-passwall=n" >> ./.config
+#	echo "CONFIG_PACKAGE_luci-app-ssr-plus=y" >> ./.config
+#	echo "CONFIG_PACKAGE_luci-app-turboacc=y" >> ./.config
+#else
+#	echo "CONFIG_PACKAGE_luci=y" >> ./.config
+#	echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
+#	echo "CONFIG_PACKAGE_luci-app-homeproxy=y" >> ./.config
+#fi
